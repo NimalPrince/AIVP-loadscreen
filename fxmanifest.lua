6973e9b42721340c3b1989792473e7ad60c617de
DedSec <75977900+NimalPrince@users.noreply.github.com>
2023-01-14 14:06:18 +0530

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'NP'
+description 'AIVP loading screen'
+
+files {
+    '*.html',
+    'assets/**/*.*',
+    'assets/**/**/*.*'
+}
+
+client_script 'client.lua'
+
+--loadscreen_manual_shutdown "yes"
+loadscreen 'index.html'
